38cbf63de63345cd7a8215c666984e00054043c3
mikellarrubis <mikel_l@professy.co.jp>
2021-04-27 16:52:03 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [fields, setFields] = useState({ endpoint: '', token: '', projectId: 0 })
+
+  const handleLoadDetails = async () => {
+    console.log(`Request: ${fields}`)
+    const {data} = await axios.post(fields.endpoint, { token: fields.token, projectId: fields.projectId })
+    
+    console.log(`Response: ${data}`)
+  }
+
+  const handleChange = (e) => {
+    e.persist()
+
+    setFields((prevState) => {
+      return { ...prevState, [e.target.id]: e.target.value.trim() }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="App_Title">EC_Page</p>
+      <div className="App_Response">Check inspect element for the response</div>
+      <div className="App_Details">
+        <div className="App_DetailFileds">
+          <div className="App_Field">
+            <label htmlFor="endpoint" className="App_Label">Endpoint: </label>
+            <input
+              className="App_Input"
+              type="text"
+              id="endpoint"
+              value={fields.endpoint}
+              onChange={handleChange}
+            />
+          </div>
+          <br/>
+          <div className="App_Field">
+            <label htmlFor="token" className="App_Label">Token: </label>
+            <input
+              className="App_Input"
+              type="text"
+              id="token"
+              value={fields.token}
+              onChange={handleChange}
+            />
+          </div>
+          <br/>
+          <div className="App_Field">
+            <label htmlFor="projectId" className="App_Label">Project Id: </label>
+            <input
+              className="App_Input"
+              type="number"
+              id="projectId"
+              value={fields.projectId}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="App_Button" onClick={() => handleLoadDetails()}>Load</div>
     </div>
-  );
+  )
 }
 
 export default App;
