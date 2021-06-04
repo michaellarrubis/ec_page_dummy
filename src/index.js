c1c009b7b0482e4bfb44c8c27cf7b537f160537b
mikellarrubis <mikel_l@professy.co.jp>
2021-06-04 20:35:14 +0800

    add cohere-js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Cohere from "cohere-js";
+Cohere.init("blkQIrjdkImnPqWICXznRkZc");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
